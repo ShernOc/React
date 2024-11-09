30524c338b31e68c91e910d362b6f93da737d834
ShernOC <sherlynea1994@hotmail.com>
2024-11-09 12:52:09 +0300

    added prop component

--- react-demo/src/comp1/App.jsx
+++ react-demo/src/comp1/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
-import Intro from './Intro'
+import IntroReact from './IntroReact'
 import Header from './Header'
 import Footer from './Footer'
 import Contact from './Contact'
 import About from './About'
+import Props from './Props'
 
 function App() {
   return (
     <> 
     <Header/>
     <About/>
-    <Intro/>
+    <IntroReact/>
+    <Props 
+    title = "WHAT ARE PROPS?: displayed as a prop" 
+    display = "props in curly braces"/>
     <Contact /><br />
     <Footer/>
     
---
+++ react-demo/src/comp1/IntroReact.jsx
@@ -0,0 +1,21 @@
+
+function IntroReact() {
+    return (
+        <div id='container'>
+            <h2>
+                REACT COMPONENTS
+            </h2>
+            <h3>What are React Components?</h3>
+            <p>React component is a function. To  create a react component,
+                <ol>
+                    <li>1. We create a  function and name it, and return a jsx syntax like code.</li>
+                    <li> 2. There are <b>Imports </b> and <b>Exports </b> That allows one to move/reuse and access functions within the entire application or project.</li><br />
+                    <li>3.We have rules in React, such a. Return multiple elements wrapped in a single div tag or fragment tag. b. Have all the JSX tag be closed and c. Have ClassName be in Camelcase and Javascript functions should start in small letter to differentiate with React functions.</li>
+                </ol>
+                <br />
+                This is mind blowing, and i Cant Wait till I learn how to now have Javascript dynamic functionality be integrated into React </p>
+        </div>
+    );
+}
+
+export default IntroReact;
